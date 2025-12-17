332d68a56b1e109ac2e798c01f57deed6c3183a9
shahxaib2117-cyber <shahxaib2117@gmail.com>
2025-12-17 12:45:35 +0500

    dropdown problem fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import logo from '../assets/logo.png'
 import { IoIosArrowDown } from "react-icons/io";
 import Button from './commons/Button';
@@ -7,12 +7,21 @@ import { IoMenuOutline } from "react-icons/io5";
 import Modal from './commons/Modal';
 
 const Navbar = () => {
-
+    // toggle for menu button
     const [toggle, setToggle] = useState(false)
-    console.log("🚀 ~ Navbar ~ toggle:", toggle)
-
+    // dropdownid for dropdown
     const [dropDownId, setDropDownId] = useState(null)
+    // isclicked for render menu
+    const [clasName, setClasName] = useState(null)
 
+    const handleSetClass = () => {
+        if (clasName === "slide-off" || clasName === null) {
+            setClasName("slide-on")
+        } else {
+            setClasName("slide-off")
+        }
+        setToggle(!toggle)
+    }
 
     return (
         <div className=" w-full flex flex-col ">
@@ -22,29 +31,29 @@ const Navbar = () => {
                     <div className={`${toggle ? "transition-all button-shadow duration-200 p-1 rounded-full " :
                         " "
                         } `}>
-                        <IoMenuOutline onClick={() => setToggle(!toggle)} className={` text-[25px] text-[#1f5898] `} />
+                        <IoMenuOutline onClick={() => handleSetClass()} className={` text-[25px] text-[#1f5898] `} />
                     </div>
                 </div>
                 <div className="h-12 w-36 tablet:19 tablet:w-60 p-1 ml-5 tablet:ml-10 tablet:h-18 ">
                     <img src={logo} className='h-full ' alt="" />
                 </div>
                 {/* LINKS */}
-                <div className="h-full hidden flex-1 laptop:flex justify-around items-center gap-5 px-20  ">
+                <div className="h-full hidden flex-1 laptop:flex justify-around items-center gap-5 px-20 ">
                     {
                         links.map((data, index) => (
-                            <div key={index} className='relative '>
-                                <a href={data.path} onMouseEnter={() => setDropDownId(index)} onMouseLeave={() => setDropDownId(null)} className=" flex justify-between items-center gap-1 group">
+                            <div key={index} onMouseEnter={() => setDropDownId(index)} onMouseLeave={() => setDropDownId(null)} className='relative '>
+                                <a href={data.path} className=" flex justify-between items-center gap-1">
                                     {/* text */}
                                     <div className="text-[14px] font-semibold whitespace-nowrap ">{data.text}</div>
                                     {/* dropdown icon */}
                                     {data?.dropdown?.status ?
-                                        <IoIosArrowDown className='mt-1 transition-all duration-500 group-hover:rotate-180 ' />
+                                        <IoIosArrowDown className={`${dropDownId === index && "rotate-180"} mt-1 transition-all duration-500 `} />
                                         :
                                         <div className=""></div>
                                     }
                                 </a>
-                                <div className={`duration-500 overflow-hidden rounded-md bg-[#ebebeb] px-3 border-[#acc0f3] ${(data?.dropdown?.status && dropDownId === index) ?
-                                    " max-h-32 opacity-100 " : " max-h-0 opacity-0 "}  border-2 absolute p-2 `}>
+                                <div className={`duration-500 overflow-hidden rounded-md bg-[#ebebeb] px-3 border-[#acc0f3] absolute 
+                                    ${(data?.dropdown?.status && dropDownId === index) ? " max-h-32 opacity-100 " : " max-h-0 opacity-0 "}  border-2 absolute p-2 `}>
                                     {
                                         data.dropdown?.dataa?.map((dat, ind) => (
                                             <p className='whitespace-nowrap text-[#205796] ' key={ind}>{dat.miniLink}</p>
@@ -60,7 +69,8 @@ const Navbar = () => {
                     <Button lable={"Contact Us"} />
                 </div>
             </div>
-            <div className={` w-full bg-slate-300 z-40 fixed flex flex-col justify-center items-center py-5 gap-2 mt-14 tablet:mt-19 ${toggle ? "slide-on" : "slide-off"}  `}>
+            <div className={` w-full bg-slate-300 z-40 fixed flex flex-col justify-center items-center lg:hidden py-5 gap-2 mt-14 tablet:mt-19 
+            ${clasName == null ? "hidden" : clasName}  `}>
                 {
                     links.map((data, index) => (
                         <a href={data.path} key={index} className=" flex justify-between items-center p-1 gap-1 ">
@@ -71,7 +81,7 @@ const Navbar = () => {
                 }
             </div>
             {/* <div className={` h-30 w-full bg-slate-600  `}></div> */}
-        </div>
+        </div >
     )
 }
 
